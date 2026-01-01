5252644546e61eefdbd9eff645551e02f4b9d5b5
Szchiji <118823636+Szchiji@users.noreply.github.com>
2026-01-01 23:03:00 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 db = SQLAlchemy()
+global_bot = None
+global_loop = None
 
 def create_app():
     app = Flask(__name__)
     
-    # 配置
     db_uri = os.getenv('DATABASE_URL', 'sqlite:///bot.db')
-    if db_uri.startswith("postgres://"):
+    if db_uri and db_uri.startswith("postgres://"):
         db_uri = db_uri.replace("postgres://", "postgresql://", 1)
         
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
@@ -18,7 +19,6 @@ def create_app():
     
     db.init_app(app)
     
-    # 注册蓝图/路由
     from .web_routes import web_bp
     app.register_blueprint(web_bp)
     
