cc3d88910de35f42caeb0d5296a6db1f6979c738
Szchiji <118823636+Szchiji@users.noreply.github.com>
2026-01-02 01:07:27 +0800

    Create services.py

---
+++ app/services.py
@@ -0,0 +1,14 @@
+from .models import Config, db
+import json
+
+def get_conf(key, default):
+    c = Config.query.filter_by(key=key).first()
+    return json.loads(c.value) if c else default
+
+def set_conf(key, value):
+    c = Config.query.filter_by(key=key).first()
+    if not c:
+        db.session.add(Config(key=key, value=json.dumps(value, ensure_ascii=False)))
+    else:
+        c.value = json.dumps(value, ensure_ascii=False)
+    db.session.commit()
